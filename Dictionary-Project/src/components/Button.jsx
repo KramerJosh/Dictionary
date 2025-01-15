0e5554c4c2c82d8757d17c8860ca643dfae5264d
Clouser <clouser24ryan@gmail.com>
2025-01-14 20:26:00 -0500

    added button

---
+++ Dictionary-Project/src/components/Button.jsx
@@ -0,0 +1,9 @@
+function Button(label, destination) {
+    return(
+        <>
+            <button className="quiz-button" onClick={console.log("test")}>{label}</button>
+        </>
+    )
+}
+
+export default Button
