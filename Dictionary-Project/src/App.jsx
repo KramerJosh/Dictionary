d06014d07f001926e4f2652a1419503d91e4dccd
Josh Kramer <kramer@nyu.edu>
2025-01-14 20:41:45 -0500

    did some button work

--- Dictionary-Project/src/App.jsx
+++ Dictionary-Project/src/App.jsx
@@ -4,6 +4,7 @@ import Header from './components/Header'
 
 function App() {
   const [count, setCount] = useState(0)
+  const title = 'Test'
 
   return (
     <>
--- Dictionary-Project/src/components/Header.jsx
+++ Dictionary-Project/src/components/Header.jsx
@@ -1,4 +1,4 @@
-function Header(title) {
+function Header({title}) {
     return(
         <>
             <h1 className="title">{title}</h1>
